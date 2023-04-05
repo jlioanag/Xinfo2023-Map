27500c829dd603b2f97f28cd9ca745949f0a8e1e
jlioanag <40252721+jlioanag@users.noreply.github.com>
2023-04-05 09:42:22 -0400

    add country outline

---
+++ map.js
@@ -0,0 +1,9 @@
+
+var map = L.map('map').setView([4.038, 21.758], 3);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.geoJson(africa).addTo(map);
