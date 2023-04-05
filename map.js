cfb127db27ee9b90e68538cee83ee0efccfbc58b
jlioanag <40252721+jlioanag@users.noreply.github.com>
2023-04-05 11:21:42 -0400

    updated country boundaries to be more legible

--- map.js
+++ map.js
@@ -3,7 +3,20 @@ var map = L.map('map').setView([4.038, 21.758], 3);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
+    minZoom: 3,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.geoJson(africa).addTo(map);
+function style(feature) {
+    return {
+        fillColor: 'transparent',
+        weight: 2,
+        opacity: 1,
+        color: 'black',
+        dashArray: '7',
+        fillOpacity: 0.7
+    };
+}
+
+
+L.geoJson(africa, {style: style}).addTo(map);
