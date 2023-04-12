9b366388b1086f64d46dde36366c707054f5f479
DefinitiveAbove <definitiveabove@gmail.com>
2023-04-12 10:35:50 -0400

    merge

--- map.js
+++ map.js
@@ -18,5 +18,58 @@ function style(feature) {
     };
 }
 
-
 L.geoJson(africa, {style: style}).addTo(map);
+
+let countriesLayer = L.geoJson(africa).addTo(map);
+
+let layerDict = {};
+
+console.log(countriesLayer);
+
+countriesLayer.eachLayer((layer) => {
+    let layerID = countriesLayer.getLayerId(layer);
+    if (layer.feature.properties.iso_a3 == "-99") {
+        layerDict[layer.feature.properties.sov_a3] = layerID;
+        // layerDict[layer.feature]
+    } else {
+        layerDict[layer.feature.properties.iso_a3] = layerID;
+    }
+
+
+    layer.setStyle({
+        // color: "#FFFFFF",
+        stroke: false,
+        // fillColor: "#000000"
+        fillColor: "hsl(250, 0%, 50%)"
+    })
+});
+
+let colorToHue = {
+    "red": 0,
+    "green": 100,
+    "blue": 240,
+    "pink": 300,
+    "yellow": 60,
+    "purple": 280
+}
+
+let highlightSelectedCountries = (countryData, hue) => {
+    countriesLayer.eachLayer((layer) => {
+        let layerCountryCode = layer.feature.properties.iso_a3;
+        if (layer.feature.properties.iso_a3 == "-99") {
+            layerCountryCode = layer.feature.properties.sov_a3;
+        }
+        
+        let saturation = 0
+        if (layerCountryCode in countryData) {
+            saturation = countryData[layerCountryCode];
+        }
+        saturation = Math.random() * 100;
+        layer.setStyle({
+            stroke: false,
+            fillColor: "hsl(" + hue + ", " + saturation + "%, 50%)"
+        });
+    })
+}
+
+highlightSelectedCountries({}, 0)
